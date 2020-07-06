aac7ce66681294ed4bd111ca513a3c1b174c0177
Nagaharish <naghareesh@gmail.com>
2020-07-06 16:03:10 +0530

    commited

---
+++ application.java
@@ -0,0 +1,12 @@
+git branch bn
+git tag tn
+git branch
+git tag
+git checkout bn
+git branch -d bn
+git tag -d tn
+git push origin --all
+git push origin --tags
+git push origin --delete bn
+git branch -a
+
